d982932eb2d4cfade0e4ba921c4df9844747cb9a
Marcos Davi A. Feitosa <89201085+marcosafeitosa@users.noreply.github.com>
2024-12-08 17:17:12 -0300

    Update and rename funcaoCompartilhada.js to utils.js

--- funcaoCompartilhada.js
+++
@@ -1,4 +0,0 @@
-// commons.js
-export function funcaoCompartilhada() {
-    console.log('Função compartilhada chamada!');
-}
---
+++ utils.js
@@ -0,0 +1,8 @@
+// utils.js
+export function saudacao(nome) {
+    return `Olá, ${nome}!`;
+}
+
+export function soma(a, b) {
+    return a + b;
+}
